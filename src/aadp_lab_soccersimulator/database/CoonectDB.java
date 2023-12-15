928a14dc3f7d2d4640d72c7cf4343c572a2562ab
Carolina <carolina.gomeslandim@gmail.com>
2023-12-15 13:34:35 +0000

    Updates CreateDB, ConnectDB

--- src/aadp_lab_soccersimulator/database/CoonectDB.java
+++ src/aadp_lab_soccersimulator/database/CoonectDB.java
@@ -4,10 +4,34 @@
  */
 package aadp_lab_soccersimulator.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 /**
  *
- * @author carol
+ * @author Carolina Landim 2021226
  */
 public class CoonectDB {
     
+    
+    // Connect the Database
+    public Connection ConnectDB(){
+        
+        Connection conn = null;
+        
+        try{
+
+            String dbName = "world_cup";
+            String DB_URL = "jdbc:mysql://localhost/" + dbName;
+            String USER = "football";
+            String PASS = "Java is almost as good as football";
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/", USER, PASS);
+        }catch(SQLException e){
+            System.out.println("Error CreateDB" + e);
+        }
+        return conn;
+}
 }
--- src/aadp_lab_soccersimulator/database/InsertDatabase.java
+++ src/aadp_lab_soccersimulator/database/InsertDatabase.java
@@ -4,10 +4,38 @@
  */
 package aadp_lab_soccersimulator.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import teams.Players;
+
 /**
  *
- * @author carol
+ * @author Carolina Landim 2021226
  */
 public class InsertDatabase {
+    
+    public void InsertDatabase(Players objplayer){
+        
+        Connection conn = null;
+        String teamName = null;
+        
+        String sql = "INSERT INTO " + teamName + " (name, number, birth, position, goalsScored, background) VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            // Set parameters for the prepared statement
+            pstmt.setString(1, objplayer.getName());
+            pstmt.setInt(2, objplayer.getNumber());
+            pstmt.setString(3, objplayer.getBirth());
+            pstmt.setString(4, objplayer.getPosition());
+            pstmt.setInt(5, objplayer.getGoalsScored());
+            pstmt.setString(6,objplayer.getBackground());
+
+            // Execute the update
+            pstmt.executeUpdate();
+        
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
