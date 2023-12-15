906605f1189424d7a4ea2e5301aa47b1080cbde3
wandyalmeida <77290358+wandyalmeida@users.noreply.github.com>
2023-12-15 14:12:06 +0000

    add first menu

---
+++ src/FirstMenu/initialScreen.java
@@ -0,0 +1,40 @@
+package FirstMenu;
+
+import java.util.Scanner;
+import questions.Questions;
+
+/**
+ *
+ * @author wandw
+ */
+public class initialScreen {
+    Scanner sc = new java.util.Scanner(System.in);
+    int option;
+    Questions questions = new Questions();
+    
+    public void startMenu(){
+        System.out.println("Welcome! What would you like to do today? Please enter the number corresponding to your choice.");
+        System.out.println("1. Enter a new player to a team.");
+        System.out.println("2. See the players on a team.");
+        System.out.println("3. Simulate a number of matches.");
+        System.out.println("4. Exit the programme."); 
+        option = sc.nextInt();
+        
+        switch (option){
+            case 1:
+                questions.userInput();
+                    
+            case 2:
+             
+            case 3:
+            
+            case 4:
+                
+            default:
+                
+            
+        }
+        
+    }
+    
+}
