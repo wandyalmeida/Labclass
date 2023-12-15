4aaa84cea27406ba7b3e4d713aecd634b79d29e4
Carolina <carolina.gomeslandim@gmail.com>
2023-12-15 14:20:06 +0000

    Get the code and insert in the classes

--- src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
+++ src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
@@ -39,25 +39,7 @@ public class AADP_Lab_SoccerSimulator {
      */
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         
-         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", USER, PASS);
-            Statement stmt = conn.createStatement();
-            stmt.execute("CREATE SCHEMA IF NOT EXISTS " + dbName +";");
-            stmt.execute("USE " + dbName + ";");
-            for (String team : teams) {
-                stmt.execute(
-                        "CREATE TABLE IF NOT EXISTS "+ team + " ("
-                                + "name VARCHAR(30) NOT NULL,"
-                                + "number INT NOT NULL PRIMARY KEY,"
-                                + "birth VARCHAR(30),"
-                                + "position VARCHAR(30),"
-                                + "goalsScored INT,"
-                                + "background TEXT(1000));"
-                );                
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        
         int option;
         boolean exit = false;
         Scanner sc = new Scanner(System.in);
@@ -143,30 +125,7 @@ public class AADP_Lab_SoccerSimulator {
                         if (teamName.toLowerCase().equals("exit")) break;
                         if (!validTeam) System.out.println("That is not one of the teams. Please try again!");
                     } while (!validTeam);
-                    try {
-                        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                        Statement stmt = conn.createStatement();
-                        ResultSet rs = stmt.executeQuery("SELECT * from " + teamName + ";");
-                        String name;
-                        int number;
-                        String birth;
-                        String position;
-                        int goalsScored;
-                        String background;
-                        while (rs.next()) {
-                            name = rs.getString("name");
-                            number = rs.getInt("number");
-                            birth = rs.getString("birth");
-                            position = rs.getString("position");
-                            goalsScored = rs.getInt("goalsScored");
-                            background = rs.getString("background");              
-                            System.out.println(String.format("Name: %s -- Number: %d -- DoB: %s -- Position: %s -- Number of goals scored: %d", name, number, birth, position, goalsScored));
-                            System.out.println("Background:");
-                            System.out.println(background);
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }                     
+                                    
                 } else if (option == 3) {
                     System.out.println("How many matches would you like to simulate?");
                     Boolean validNum = false;
