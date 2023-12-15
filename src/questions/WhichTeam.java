8f147f41e21069345a634482edd0cb8e1298e365
Charles Rocha <charlesmsrocha@gmail.com>
2023-12-15 14:26:04 +0000

    last commit

--- src/questions/WhichTeam.java
+++ src/questions/WhichTeam.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package questions;
+
+import FirstMenu.initialScreen;
+import java.util.Scanner;
+import teams.Teams;
 
 /**
  *
  * @author Charles Rocha
  */
 public class WhichTeam {
+    
+    Teams nations = new Teams();
+    Scanner nationsInput = new Scanner(System.in);
+    initialScreen firstScreen = new initialScreen();
+    
+    try{
+            System.out.println("Please follow the instructions to enter player data.");
+        do{
+            System.out.println("For which team would you like to enter data?");
+            nationInput
+            
+        }
+}catch{
+    
+}
     
 }
