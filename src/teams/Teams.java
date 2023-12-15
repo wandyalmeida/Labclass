10d4884b1ca4a448700f4de857358e94109a7cd7
Charles Rocha <charlesmsrocha@gmail.com>
2023-12-15 13:04:31 +0000

    Teams and players classes

---
+++ src/teams/Teams.java
@@ -0,0 +1,11 @@
+package teams;
+
+/**
+ *
+ * @author Charles Rocha 2021376
+ */
+public class Teams {
+
+    String[] teams = {"Ireland", "Brazil", "Argentina", "Japan", "Mexico", "Senegal", "Tunisia", "Qatar"};
+
+}
